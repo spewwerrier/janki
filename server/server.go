ec503e670f194ac55a3ad9cc96cb135e045c5b4c
Aagaman Luitel <mail@luitelaagaman.com.np>
2024-09-14 22:15:06 +0545

    create table when starting the server

--- server/server.go
+++ server/server.go
@@ -3,11 +3,16 @@ package server
 import (
 	"janki/api"
 	"janki/db"
+	"log"
 	"net/http"
 )
 
 func Server() {
 	database := db.NewConnection("user=janki dbname=janki password=janki sslmode=disable port=5555")
+	err := database.Create_db()
+	if err != nil {
+		log.Panic(err)
+	}
 
 	api := &api.Api{
 		Users: api.Users{DB: database},
@@ -18,5 +23,10 @@ func Server() {
 		Addr:    "localhost:8080",
 		Handler: Handler(api),
 	}
-	_ = s.ListenAndServe()
+	log.Println("listening on http://localhost:8080")
+	err = s.ListenAndServe()
+	if err != nil {
+		log.Panic(err)
+		s.Close()
+	}
 }
