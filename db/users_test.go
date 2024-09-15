c565eb5db712e415be66d9951a8623392758d8db
Aagaman Luitel <mail@luitelaagaman.com.np>
2024-09-15 20:55:11 +0545

    proper tests

--- db/users_test.go
+++ db/users_test.go
@@ -1,22 +1,59 @@
 package db
 
 import (
-	"fmt"
 	"testing"
 )
 
 func TestGetUsers(t *testing.T) {
-	database := NewConnection("user=janki_test dbname=janki_test password=janki_test sslmode=disable port=5556")
+	db := NewConnection("user=janki_test dbname=janki_test password=janki_test sslmode=disable port=5556")
 
-	err := database.Create_db()
+	err := db.Create_db()
+	defer db.db.Close()
 	if err != nil {
 		t.Fatal(err)
 	}
 
-	s, err := database.RetriveUser("someradnomcookiestring")
+	_, err = db.CreateNewUser("dummyuser", "dummy password", "groot", "groot")
 	if err != nil {
 		t.Fatal(err)
 	}
-	fmt.Printf("t: %v\n", s)
-	fmt.Println(s)
+
+	_, err = db.CreateNewUser("dummyuser", "different password", "groot", "groot")
+	if err == nil {
+		t.Fatal("should complain about duplicate user but did not")
+	}
+
+	_, err = db.CreateNewUser("dummyuser2", "different password", "groot", "groot")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+}
+
+func TestUserDescriptions(t *testing.T) {
+	db := NewConnection("user=janki_test dbname=janki_test password=janki_test sslmode=disable port=5556")
+
+	err := db.Create_db()
+	defer db.db.Close()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	session_key, err := db.CreateNewUser("spw", "spewed everywhere", "bleh", "bleh")
+	if err != nil {
+		t.Fatal(err)
+	}
+	second_key, err := db.RetriveUserSession("spw", "spewed everywhere")
+	if err != nil {
+		t.Fatal(err)
+	}
+	if session_key != second_key {
+		t.Fatal(err)
+	}
+
+	err = db.UpdateUser(session_key, "zahallo", "zahallo")
+	if err != nil {
+		t.Fatal(err)
+	}
+
 }
