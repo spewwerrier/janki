e44832a2c93da737260e6429e9449ed63e9d6f3b
Aagaman Luitel <mail@luitelaagaman.com.np>
2024-10-15 18:50:42 +0545

    update logic

---
+++ db/update_knob.go
@@ -0,0 +1,19 @@
+package db
+
+import "fmt"
+
+// TODO: check if api and identifier belongs to same user
+func (db *Database) UpdateKnob(api_key string, identifier string, key string, value string) error {
+	knob_id, err := db.GetKnobIdFromIdentifier(identifier)
+	if err != nil {
+		db.log.Error("UpdateKnob failed to get knob id")
+		return err
+	}
+	query := fmt.Sprintf("UPDATE knobdescriptions  SET %s = %s || ARRAY[$1]  WHERE knob_id = $2", key, key)
+	_, err = db.raw.Exec(query, value, knob_id)
+	if err != nil {
+		db.log.Error("UpdateUser: Failed to update user " + err.Error())
+		return err
+	}
+	return nil
+}
---
+++ db/update_user.go
@@ -0,0 +1,20 @@
+package db
+
+import "fmt"
+
+func (db *Database) UpdateUser(api_key string, key string, value string) error {
+	query := fmt.Sprintf("UPDATE usersdescriptions set %s = $1 where user_id  = $2", key)
+	id, err := db.GetUserId(api_key)
+	fmt.Println(id)
+	if err != nil {
+		db.log.Error("UpdateUser: Failed to get user id " + err.Error())
+		return err
+	}
+	_, err = db.raw.Exec(query, value, id)
+	fmt.Println(query, value, id)
+	if err != nil {
+		db.log.Error("UpdateUser: Failed to update user " + err.Error())
+		return err
+	}
+	return nil
+}
